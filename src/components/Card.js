5ec04e96b6e8cdf9f015c44d45bbc99fcb42eed6
Stephen5124 <stephen.buckley94@gmail.com>
2023-12-22 05:38:09 +0000

    Implemented game logic with card shuffling, match handling, game reset, and shuffle animation re-triggering

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import '../styles/Card.scss';
 
-function Card({ id, isFlipped, onCardClick }) {
+function Card({ id, isFlipped, onCardClick, value }) {
   return (
     <div className={`card-container ${isFlipped ? 'flipped' : ''}`} onClick={() => onCardClick(id)}>
       <div className="card-front">
-        Front 
+        {/* You might eventually want to display something based on the card's value */}
+        Front {value}
       </div>
       <div className="card-back">
-        Back 
+        Back
       </div>
     </div>
   );
--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,36 +1,100 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import Card from './Card';
 import '../styles/GameBoard.scss';
 
 function GameBoard() {
+  const [cards, setCards] = useState([]);
   const [flippedCards, setFlippedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+  const [isShuffling, setIsShuffling] = useState(true);
+  const [resetCount, setResetCount] = useState(0);
+
+  // Fisher-Yates Shuffle Algorithm
+  const shuffleCards = (array) => {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  };
+
+  // Memoize resetGame using useCallback
+  const resetGame = useCallback(() => {
+    setIsShuffling(true);
+    setFlippedCards([]);
+    setMatchedCards([]);
+    setCards(shuffleCards([...Array(30).keys()].flatMap(i => [i, i])));
+
+    const shuffleDuration = 60 * 50; // Adjust based on your animation
+    setTimeout(() => {
+      setIsShuffling(false);
+    }, shuffleDuration);
+    setResetCount(count => count + 1);
+  }, []); // Empty dependency array means the function is created only once
+
+  // Shuffle cards on component mount
+  useEffect(() => {
+    resetGame();
+  }, [resetGame]);
 
   const handleCardClick = (cardId) => {
-    if (flippedCards.length < 2 && !flippedCards.includes(cardId)) {
-      setFlippedCards([...flippedCards, cardId]);
+    if (isShuffling || flippedCards.length === 2 || flippedCards.includes(cardId) || matchedCards.includes(cardId)) {
+      return; // Ignore clicks under certain conditions
+    }
+
+    const newFlippedCards = [...flippedCards, cardId];
+    setFlippedCards(newFlippedCards);
+
+    if (newFlippedCards.length === 2) {
+      const match = cards[newFlippedCards[0]] === cards[newFlippedCards[1]]; // Check if the flipped cards match
+      if (match) {
+        setMatchedCards([...matchedCards, ...newFlippedCards]);
+      }
+      setTimeout(() => {
+        setFlippedCards([]); // Reset flipped cards
+      }, 1000);
     }
   };
 
   useEffect(() => {
-    if (flippedCards.length === 2) {
+    if (flippedCards.length === 2 && !matchedCards.includes(flippedCards[0]) && !matchedCards.includes(flippedCards[1])) {
       setTimeout(() => {
-        setFlippedCards([]);
-      }, 3000);
+        setFlippedCards([]); // Reset flipped cards if not a match
+      }, 1000);
     }
-  }, [flippedCards]);
+  }, [flippedCards, matchedCards, cards]);
 
   return (
-    <div className="game-board">
-      {Array.from({ length: 24 }).map((_, idx) => (
-        <Card 
-          key={idx}
-          id={idx}
-          isFlipped={flippedCards.includes(idx)}
-          onCardClick={() => handleCardClick(idx)}
-        />
-      ))}
+    <div>
+      <button onClick={resetGame} disabled={isShuffling}>Reset Game</button>
+      <div className="game-board">
+        {cards.map((card, idx) => (
+          <div 
+            key={`${resetCount}-${idx}`} // Change the key to force re-render
+            style={{ '--card-index': idx }}
+            className="card-container"
+          >
+            <Card 
+              id={idx}
+              value={card}
+              isFlipped={flippedCards.includes(idx) || matchedCards.includes(idx)}
+              onCardClick={() => handleCardClick(idx)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  );
+  );  
 }
 
 export default GameBoard;
